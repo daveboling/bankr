2fe03e583f9285e5f339567f9b8a48331cb1980d
David Boling <daviddboling@gmail.com>
2014-08-09 22:44:51 -0500

    Added most of the account model

--- app/models/account.js
+++ app/models/account.js
@@ -1,1 +1,99 @@
 'use strict';
+
+var Mongo = require('mongodb');
+var _ = require('lodash');
+
+function Account(obj){
+  this.name           = obj.name;
+  this.color          = obj.color;
+  this.pin            = obj.pin;
+  this.photo          = obj.photo;
+  this.balance        = parseFloat(obj.balance);
+  this.accountType    = obj.accountType;
+  this.openDate       = new Date(obj.openDate);
+  this.transactions   = [];
+}
+
+
+//Getter for database collection
+Object.defineProperty(Account, 'collection', {
+  get: function(){return global.mongodb.collection('accounts');}
+});
+
+
+Account.prototype.newTransaction = function(trans, cb){
+  var fee = 0;
+  var amount = parseFloat(trans.amount);
+  if(trans.type === 'withdraw'){
+   this.balance -= amount;
+    if(this.balance < 0){
+     fee = 50;
+     this.balance -= fee;
+    }
+  }
+  else {
+   this.balance += amount;
+  }
+
+  var transaction = {type: trans.type, amount: trans.amount, date: new Date(), id: this.transactions.length+1, fee: fee};
+  this.transactions.push(transaction);
+  //will callback newTransaction when complete so it can finish
+  Account.collection.update({_id:this._id}, {$push:{transactions:transaction}}, cb);
+
+};
+
+Account.create = function(account, cb){
+  var newAccount  = account;
+  Account.collection.save(newAccount, cb);
+};
+
+
+//Needs to have transfers added as well
+Account.display = function(query, cb){
+  var id = Mongo.ObjectID(query);
+  Account.collection.findOne({_id: id}, function(err, obj){
+    var account = changeProto(obj);
+    //pre sort array in
+    sortDateAscending(account.transactions);
+    cb(account);
+  });
+};
+
+
+Account.findById = function(query, cb){
+  var id = Mongo.ObjectID(query);
+  Account.collection.findOne({_id: id}, function(err, obj){
+    var account = changeProto(obj);
+    cb(account);
+  });
+};
+
+
+
+// HELPER FUNCTIONS
+function changeProto(obj){
+  return _.create(Account.prototype, obj);
+};
+
+function sortDateAscending(arr){
+  arr.sort(function(a,b){
+    return new Date(b.date) - new Date(a.date);
+  });
+}
+
+
+
+module.exports = Account; 
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/scripts/freshdb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-../../../../../mongodb/bin/mongoimport --drop --db test --collection transactions --file ../../db/transactions.json
